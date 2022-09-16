34b77f8bbfbe80ebccb8c8a57e78d231e06fca1e
Mark A. Greenslade <asladeofgreen@pm.me>
2022-09-16 23:54:28 +0200

    1. Exxtened gitignore.

--- write.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source $HOME/Work/ipsl/ipsl.esdoc/pyessv-writers/activate
-pyessv-write-all \
-    $HOME/Work/ipsl/ipsl.esdoc/other/CMIP6_CVs \
-    $HOME/Work/ipsl/ipsl.esdoc/other/esg-config/publisher-configs/ini
